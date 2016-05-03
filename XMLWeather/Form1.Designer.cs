c615608e621842634b902c6eeac620198642214b
Liam Westman <liamwest416@@ed.amdsb.ca>
2016-05-03 10:17:20 -0400

    Weather Final

---
+++ XMLWeather/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace XMLWeather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cityOutput = new System.Windows.Forms.Label();
+            this.currentTempOut = new System.Windows.Forms.Label();
+            this.windDescOut = new System.Windows.Forms.Label();
+            this.maxOutput = new System.Windows.Forms.Label();
+            this.minOutput = new System.Windows.Forms.Label();
+            this.max2Output = new System.Windows.Forms.Label();
+            this.min2Output = new System.Windows.Forms.Label();
+            this.day1Clouds = new System.Windows.Forms.Label();
+            this.day2Clouds = new System.Windows.Forms.Label();
+            this.weatherTitle = new System.Windows.Forms.Label();
+            this.tempOutput = new System.Windows.Forms.Label();
+            this.windOutput = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tommorowTemp = new System.Windows.Forms.Label();
+            this.maxLable = new System.Windows.Forms.Label();
+            this.minLabel = new System.Windows.Forms.Label();
+            this.windDescOut2 = new System.Windows.Forms.Label();
+            this.precLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cityOutput
+            // 
+            this.cityOutput.AutoSize = true;
+            this.cityOutput.BackColor = System.Drawing.Color.Transparent;
+            this.cityOutput.Font = new System.Drawing.Font("Open Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cityOutput.Location = new System.Drawing.Point(23, 50);
+            this.cityOutput.Name = "cityOutput";
+            this.cityOutput.Size = new System.Drawing.Size(42, 17);
+            this.cityOutput.TabIndex = 0;
+            this.cityOutput.Text = "label1";
+            // 
+            // currentTempOut
+            // 
+            this.currentTempOut.AutoSize = true;
+            this.currentTempOut.BackColor = System.Drawing.Color.Transparent;
+            this.currentTempOut.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentTempOut.Location = new System.Drawing.Point(102, 85);
+            this.currentTempOut.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.currentTempOut.Name = "currentTempOut";
+            this.currentTempOut.Size = new System.Drawing.Size(43, 18);
+            this.currentTempOut.TabIndex = 1;
+            this.currentTempOut.Text = "label1";
+            // 
+            // windDescOut
+            // 
+            this.windDescOut.AutoSize = true;
+            this.windDescOut.BackColor = System.Drawing.Color.Transparent;
+            this.windDescOut.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.windDescOut.Location = new System.Drawing.Point(103, 139);
+            this.windDescOut.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.windDescOut.Name = "windDescOut";
+            this.windDescOut.Size = new System.Drawing.Size(43, 18);
+            this.windDescOut.TabIndex = 2;
+            this.windDescOut.Text = "label1";
+            // 
+            // maxOutput
+            // 
+            this.maxOutput.AutoSize = true;
+            this.maxOutput.BackColor = System.Drawing.Color.Transparent;
+            this.maxOutput.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxOutput.Location = new System.Drawing.Point(103, 196);
+            this.maxOutput.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.maxOutput.Name = "maxOutput";
+            this.maxOutput.Size = new System.Drawing.Size(43, 18);
+            this.maxOutput.TabIndex = 3;
+            this.maxOutput.Text = "label1";
+            // 
+            // minOutput
+            // 
+            this.minOutput.AutoSize = true;
+            this.minOutput.BackColor = System.Drawing.Color.Transparent;
+            this.minOutput.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minOutput.Location = new System.Drawing.Point(102, 250);
+            this.minOutput.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.minOutput.Name = "minOutput";
+            this.minOutput.Size = new System.Drawing.Size(43, 18);
+            this.minOutput.TabIndex = 4;
+            this.minOutput.Text = "label1";
+            // 
+            // max2Output
+            // 
+            this.max2Output.AutoSize = true;
+            this.max2Output.BackColor = System.Drawing.Color.Transparent;
+            this.max2Output.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.max2Output.Location = new System.Drawing.Point(102, 196);
+            this.max2Output.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.max2Output.Name = "max2Output";
+            this.max2Output.Size = new System.Drawing.Size(43, 18);
+            this.max2Output.TabIndex = 5;
+            this.max2Output.Text = "label1";
+            // 
+            // min2Output
+            // 
+            this.min2Output.AutoSize = true;
+            this.min2Output.BackColor = System.Drawing.Color.Transparent;
+            this.min2Output.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.min2Output.Location = new System.Drawing.Point(102, 250);
+            this.min2Output.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.min2Output.Name = "min2Output";
+            this.min2Output.Size = new System.Drawing.Size(43, 18);
+            this.min2Output.TabIndex = 6;
+            this.min2Output.Text = "label2";
+            // 
+            // day1Clouds
+            // 
+            this.day1Clouds.AutoSize = true;
+            this.day1Clouds.BackColor = System.Drawing.Color.Transparent;
+            this.day1Clouds.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.day1Clouds.Location = new System.Drawing.Point(197, 234);
+            this.day1Clouds.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.day1Clouds.Name = "day1Clouds";
+            this.day1Clouds.Size = new System.Drawing.Size(43, 18);
+            this.day1Clouds.TabIndex = 7;
+            this.day1Clouds.Text = "label1";
+            this.day1Clouds.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // day2Clouds
+            // 
+            this.day2Clouds.AutoSize = true;
+            this.day2Clouds.BackColor = System.Drawing.Color.Transparent;
+            this.day2Clouds.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.day2Clouds.Location = new System.Drawing.Point(197, 234);
+            this.day2Clouds.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.day2Clouds.Name = "day2Clouds";
+            this.day2Clouds.Size = new System.Drawing.Size(43, 18);
+            this.day2Clouds.TabIndex = 8;
+            this.day2Clouds.Text = "label2";
+            // 
+            // weatherTitle
+            // 
+            this.weatherTitle.AutoSize = true;
+            this.weatherTitle.BackColor = System.Drawing.Color.Transparent;
+            this.weatherTitle.Font = new System.Drawing.Font("Open Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weatherTitle.Location = new System.Drawing.Point(111, 9);
+            this.weatherTitle.Name = "weatherTitle";
+            this.weatherTitle.Size = new System.Drawing.Size(96, 28);
+            this.weatherTitle.TabIndex = 9;
+            this.weatherTitle.Text = "Weather";
+            // 
+            // tempOutput
+            // 
+            this.tempOutput.AutoSize = true;
+            this.tempOutput.BackColor = System.Drawing.Color.Transparent;
+            this.tempOutput.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tempOutput.Location = new System.Drawing.Point(8, 85);
+            this.tempOutput.Name = "tempOutput";
+            this.tempOutput.Size = new System.Drawing.Size(89, 18);
+            this.tempOutput.TabIndex = 10;
+            this.tempOutput.Text = "Temperature:";
+            // 
+            // windOutput
+            // 
+            this.windOutput.AutoSize = true;
+            this.windOutput.BackColor = System.Drawing.Color.Transparent;
+            this.windOutput.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.windOutput.Location = new System.Drawing.Point(55, 139);
+            this.windOutput.Name = "windOutput";
+            this.windOutput.Size = new System.Drawing.Size(42, 18);
+            this.windOutput.TabIndex = 11;
+            this.windOutput.Text = "Wind:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Today",
+            "Tommorow"});
+            this.comboBox1.Location = new System.Drawing.Point(186, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(105, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tommorowTemp
+            // 
+            this.tommorowTemp.AutoSize = true;
+            this.tommorowTemp.BackColor = System.Drawing.Color.Transparent;
+            this.tommorowTemp.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tommorowTemp.Location = new System.Drawing.Point(103, 85);
+            this.tommorowTemp.Name = "tommorowTemp";
+            this.tommorowTemp.Size = new System.Drawing.Size(43, 18);
+            this.tommorowTemp.TabIndex = 13;
+            this.tommorowTemp.Text = "label1";
+            this.tommorowTemp.Visible = false;
+            // 
+            // maxLable
+            // 
+            this.maxLable.AutoSize = true;
+            this.maxLable.BackColor = System.Drawing.Color.Transparent;
+            this.maxLable.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxLable.Location = new System.Drawing.Point(23, 196);
+            this.maxLable.Name = "maxLable";
+            this.maxLable.Size = new System.Drawing.Size(74, 18);
+            this.maxLable.TabIndex = 14;
+            this.maxLable.Text = "Max Temp:";
+            // 
+            // minLabel
+            // 
+            this.minLabel.AutoSize = true;
+            this.minLabel.BackColor = System.Drawing.Color.Transparent;
+            this.minLabel.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minLabel.Location = new System.Drawing.Point(23, 250);
+            this.minLabel.Name = "minLabel";
+            this.minLabel.Size = new System.Drawing.Size(74, 18);
+            this.minLabel.TabIndex = 15;
+            this.minLabel.Text = "Min Temp: ";
+            // 
+            // windDescOut2
+            // 
+            this.windDescOut2.AutoSize = true;
+            this.windDescOut2.BackColor = System.Drawing.Color.Transparent;
+            this.windDescOut2.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.windDescOut2.Location = new System.Drawing.Point(103, 139);
+            this.windDescOut2.Name = "windDescOut2";
+            this.windDescOut2.Size = new System.Drawing.Size(43, 18);
+            this.windDescOut2.TabIndex = 16;
+            this.windDescOut2.Text = "label1";
+            // 
+            // precLabel
+            // 
+            this.precLabel.AutoSize = true;
+            this.precLabel.Location = new System.Drawing.Point(13, 9);
+            this.precLabel.Name = "precLabel";
+            this.precLabel.Size = new System.Drawing.Size(35, 13);
+            this.precLabel.TabIndex = 17;
+            this.precLabel.Text = "label1";
+            this.precLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(303, 343);
+            this.Controls.Add(this.precLabel);
+            this.Controls.Add(this.windDescOut2);
+            this.Controls.Add(this.minLabel);
+            this.Controls.Add(this.maxLable);
+            this.Controls.Add(this.tommorowTemp);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.windOutput);
+            this.Controls.Add(this.tempOutput);
+            this.Controls.Add(this.weatherTitle);
+            this.Controls.Add(this.day2Clouds);
+            this.Controls.Add(this.day1Clouds);
+            this.Controls.Add(this.min2Output);
+            this.Controls.Add(this.max2Output);
+            this.Controls.Add(this.minOutput);
+            this.Controls.Add(this.maxOutput);
+            this.Controls.Add(this.windDescOut);
+            this.Controls.Add(this.currentTempOut);
+            this.Controls.Add(this.cityOutput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Central Weather";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cityOutput;
+        private System.Windows.Forms.Label currentTempOut;
+        private System.Windows.Forms.Label windDescOut;
+        private System.Windows.Forms.Label maxOutput;
+        private System.Windows.Forms.Label minOutput;
+        private System.Windows.Forms.Label max2Output;
+        private System.Windows.Forms.Label min2Output;
+        private System.Windows.Forms.Label day1Clouds;
+        private System.Windows.Forms.Label day2Clouds;
+        private System.Windows.Forms.Label weatherTitle;
+        private System.Windows.Forms.Label tempOutput;
+        private System.Windows.Forms.Label windOutput;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label tommorowTemp;
+        private System.Windows.Forms.Label maxLable;
+        private System.Windows.Forms.Label minLabel;
+        private System.Windows.Forms.Label windDescOut2;
+        private System.Windows.Forms.Label precLabel;
+    }
+}
+
---
+++ XMLWeather/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+
+namespace XMLWeather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Text = "Today";
+       
+            // get information about current and forecast weather from the internet
+            GetData();
+
+            // take info from the current weather file and display it to the screen
+            ExtractCurrent();
+
+            // take info from the forecast weather file and display it to the screen
+            ExtractForecast();
+        }
+
+        private static void GetData()
+        {
+            WebClient client = new WebClient();
+
+            string currentFile = "http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0";
+            string forecastFile = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Stratford,CA&mode=xml&units=metric&cnt=7&appid=3f2e224b815c0ed45524322e145149f0";
+            
+            client.DownloadFile(currentFile, "WeatherData.xml");
+            client.DownloadFile(forecastFile, "WeatherData7Day.xml");
+        }
+
+        private void ExtractCurrent()
+        {
+
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("WeatherData.xml");
+
+            //create a node variable to represent the parent element
+            XmlNode parent;
+            parent = doc.DocumentElement;
+
+            //check each child of the parent element
+            foreach (XmlNode child in parent.ChildNodes)
+            {
+                // TODO if the "city" element is found display the value of it's "name" attribute
+                if (child.Name == "city")
+                {
+                    cityOutput.Text = child.Attributes["name"].Value;
+                }
+
+                if (child.Name == "temperature")
+                {
+                    currentTempOut.Text = child.Attributes["value"].Value;
+                }
+
+                if (child.Name == "wind")
+                {
+                    foreach (XmlNode grandChild in child.ChildNodes)
+                    {
+                        if (grandChild.Name == "speed")
+                        {
+                            windDescOut.Text = grandChild.Attributes["name"].Value;
+                        }
+                    }
+                }
+
+                if (child.Name == "precipitation")
+                {
+
+                    precLabel.Text = child.Attributes["mode"].Value;
+                    if (precLabel.Text == "no")
+                    {
+                        this.BackgroundImage = Properties.Resources.sunnyBackround;
+                    }
+                }
+                
+            }
+            
+        }
+
+        private void ExtractForecast()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("WeatherData7Day.xml");
+
+            //create a node variable to represent the parent element
+            XmlNode parent;
+            parent = doc.DocumentElement;
+
+            int day = 1;
+
+            //check each child of the parent element
+            foreach (XmlNode child in parent.ChildNodes)
+            {
+                if (child.Name == "forecast")
+                {
+                    foreach (XmlNode grandChild in child.ChildNodes)
+                    {
+                        foreach (XmlNode greatGrandChild in grandChild.ChildNodes)
+                        {
+                            if (greatGrandChild.Name == "windSpeed")
+                            {
+                                switch (day)
+                                {
+                                    case 1:
+                                        windDescOut.Text = greatGrandChild.Attributes["name"].Value;
+                                       
+                                        break;
+                                    case 2:
+                                        windDescOut2.Text = greatGrandChild.Attributes["name"].Value;
+                                        
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            if (greatGrandChild.Name == "temperature")
+                            {
+                                switch (day)
+                                {
+                                    case 1:
+                                        minOutput.Text = greatGrandChild.Attributes["min"].Value;
+                                        maxOutput.Text = greatGrandChild.Attributes["max"].Value; 
+                                                                
+                                        break;
+                                    case 2:
+                                        tommorowTemp.Text = greatGrandChild.Attributes["day"].Value;
+                                        min2Output.Text = greatGrandChild.Attributes["min"].Value;
+                                        max2Output.Text = greatGrandChild.Attributes["max"].Value;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }         
+                            if (greatGrandChild.Name == "clouds")
+                            {
+                                switch (day)
+                                {
+                                    case 1:
+                                        day1Clouds.Text = greatGrandChild.Attributes["value"].Value;
+                                        day++;
+                                        break;
+                                    case 2:
+                                        day2Clouds.Text = greatGrandChild.Attributes["value"].Value;
+                                        day++;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            } 
+                        }
+                    }
+                }
+            }
+        }
+
+        private void MakePictureParent()
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+     
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Today")
+            {
+                tommorowTemp.Hide();
+                windDescOut2.Hide();
+                min2Output.Hide();
+                max2Output.Hide();
+                day2Clouds.Hide();
+                maxOutput.Show();
+                minOutput.Show();
+                day1Clouds.Show();
+                currentTempOut.Show();
+                windDescOut.Show();
+            }
+            
+            if (comboBox1.Text == "Tommorow")
+            {
+                tommorowTemp.Show();
+                min2Output.Show();
+                max2Output.Show();
+                day2Clouds.Show();
+                day1Clouds.Hide();
+                currentTempOut.Hide();
+                windDescOut.Hide();
+                minOutput.Hide();
+                maxOutput.Hide();
+                windDescOut2.Show();
+            }
+        }
+    }
+}
